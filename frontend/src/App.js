c901b5a33165333046b58b924244a3ea055fd4ea
BarunBlog <bhattacharjeebarun25@gmail.com>
2024-11-27 13:22:23 +0600

    Added basic setup for todo list page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const TODO_LIST_URL = "http://127.0.0.1:8000/todo/task-list/";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const callAPI = async () => {
+      try {
+        const res = await axios.get(TODO_LIST_URL);
+        setTodos(res.data.results);
+      } catch (error) {
+        console.log("Error fetching todos:", error);
+      }
+    };
+
+    callAPI();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+
+      <div>
+        {todos.map((todo) => (
+          <div key={todo.id} className="todo-card">
+            <h2>{todo.title}</h2>
+            <p>
+              <b>status: </b>
+              {todo.status}
+            </p>
+            <br />
+            <p>{todo.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
