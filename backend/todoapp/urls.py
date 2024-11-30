5f9545b52424228b002beabb93e4284489c0bf3e
BarunBlog <bhattacharjeebarun25@gmail.com>
2024-11-30 18:53:32 +0600

    Added health check api

--- backend/todoapp/urls.py
+++ backend/todoapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskList, TaskDetail
+from .views import health_check, TaskList, TaskDetail
 
 urlpatterns = [
+    path('health/', health_check, name='health-check'),
     path('task-list/', TaskList.as_view(), name='task-list'),
     path('task-detail/', TaskDetail.as_view(), name='task-detail'),
 ]
--- backend/todoapp/views.py
+++ backend/todoapp/views.py
@@ -1,6 +1,12 @@
 from rest_framework import generics
 from .models import Task
 from .serializers import TaskSerializer
+from django.http import JsonResponse
+
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
+
 
 
 class TaskList(generics.ListCreateAPIView):
