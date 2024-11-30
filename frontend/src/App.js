b044652c23f1aeddef8f4b6da237632617205c4b
BarunBlog <bhattacharjeebarun25@gmail.com>
2024-11-30 19:19:44 +0600

    Added the server information to the app.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,14 @@ const TODO_LIST_URL = "/api/todo/task-list/";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [backendServer, setBackendServer] = useState("");
 
   useEffect(() => {
     const callAPI = async () => {
       try {
         const res = await axios.get(TODO_LIST_URL);
         setTodos(res.data.results);
+        setBackendServer(res.data.backend_server); // Capture backend info
       } catch (error) {
         console.log("Error fetching todos:", error);
       }
@@ -22,13 +24,16 @@ function App() {
   return (
     <div className="App">
       <h1>Todo List</h1>
+      <p>
+        <b>Backend Server:</b> {backendServer}
+      </p>
 
       <div>
         {todos.map((todo) => (
           <div key={todo.id} className="todo-card">
             <h2>{todo.title}</h2>
             <p>
-              <b>status: </b>
+              <b>Status: </b>
               {todo.status}
             </p>
             <br />
