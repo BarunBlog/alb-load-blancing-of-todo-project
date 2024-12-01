b0498e9c0af6ba54c8b73a60d82355315ebad422
BarunBlog <bhattacharjeebarun25@gmail.com>
2024-12-01 17:59:13 +0600

    Refactored the code

--- infra/pull_image.sh
+++ infra/pull_image.sh
@@ -5,24 +5,8 @@ FRONTEND_TAG="latest"
 BACKEND_IMAGE_NAME="todo_django_backend"
 BACKEND_TAG="latest"
 
-# Create a custom Docker network if it doesn't already exist
-if ! sudo docker network ls | grep -q my-app-network; then
-  sudo docker network create my-app-network
-  echo "Docker network 'my-app-network' created successfully."
-else
-  echo "Docker network 'my-app-network' already exists."
-fi
-
 # Pull and run the backend container on port 8000
-sudo docker run -d \
-  --network my-app-network \
-  --name backend \
-  -p 8000:8000 \
-  $DOCKER_USERNAME/$BACKEND_IMAGE_NAME:$BACKEND_TAG
+sudo docker run -d -p 8000:8000 barun25/todo_django_backend:latest
 
 # Pull and run the frontend container on port 80
-sudo docker run -d \
-  --network my-app-network \
-  --name frontend \
-  -p 80:80 \
-  $DOCKER_USERNAME/$FRONTEND_IMAGE_NAME:$FRONTEND_TAG
+sudo docker run -d -p 80:80 barun25/todo_react_frontend:latest
